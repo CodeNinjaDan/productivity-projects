e9d8d91f52950f4ed4592889079ee0a64db32be3
CodeNinjaDan <dnlmuiru@gmail.com>
2024-12-03 18:24:41 +0300

    create new cards & flip the cards

--- flash_card_app/main.py
+++ flash_card_app/main.py
@@ -1,14 +1,39 @@
 from tkinter import *
-
 import pandas
-from pandas import *
+import random
+import time
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+data = pandas.read_csv("data/french_words.csv")
+words_dict = data.to_dict(orient="records")
+current_card = {}
 
-words = pandas.read_csv("data/french_words.csv")
-words_dict = words.to_dict(orient="records")
-print(words_dict)
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(words_dict)
+    canvas.itemconfig(card_title, text= "French")
+    canvas.itemconfig(card_text, text=current_card["French"])
+    canvas.itemconfig(canvas_image, image=front_img)
+
+    # #Alternative method for picking words
+    # # Access each key with its value
+    # random_word = random.choice(words_dict)
+    # language1 = list(random_word.keys())[0]
+    # french_word = random_word[language1]
+    # canvas.itemconfig(card_title, text=language1)
+    # canvas.itemconfig(card_text, text=french_word)
+
+    flip_timer = window.after(3500, func=flip_card)
+
+def flip_card():
+    #New card configurations
+    canvas.itemconfig(canvas_image, image=back_img)
+    canvas.itemconfig(card_title, text= "English")
+    canvas.itemconfig(card_text, text=current_card["English"])
+
+
 
 # ------------------------------------ UI SETUP ------------------------------------- #
 
@@ -16,72 +41,29 @@ print(words_dict)
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3500, func=flip_card)
 
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-img = PhotoImage(file="/home/dan/PycharmProjects/productivity-projects/flash_card_app/images/card_front.png")
-canvas.create_image(400, 263, image= img)
-start_text1 = canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
-start_text = canvas.create_text(400, 263, text="trouve", font=("Ariel", 60, "bold"))
+front_img = PhotoImage(file="/home/dan/PycharmProjects/productivity-projects/flash_card_app/images/card_front.png")
+back_img = PhotoImage(file="/home/dan/PycharmProjects/productivity-projects/flash_card_app/images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image= front_img)
+
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_text = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 
-
-
-
-
-
-
-
-
-check_image = PhotoImage(file="/home/dan/PycharmProjects/productivity-projects/flash_card_app/images/known.png")
-known_button = Button(image=check_image, highlightthickness=0)
+check_image = PhotoImage(file="/home/dan/PycharmProjects/productivity-projects/flash_card_app/images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=next_card)
 known_button.grid(column=1, row=1)
 
-cross_image = PhotoImage(file="/home/dan/PycharmProjects/productivity-projects/flash_card_app/images/unknown.png")
-unknown_button = Button(image=cross_image, highlightthickness=0)
+cross_image = PhotoImage(file="/home/dan/PycharmProjects/productivity-projects/flash_card_app/images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(column=0, row=1)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+next_card()
 
 
 
